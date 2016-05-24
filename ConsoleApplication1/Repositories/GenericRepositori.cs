cbbf8e1ea066b3d2799eed2a5ea87b1afc17285b
d.ivanisevic <d.ivanisevic@DIVANISEVIC>
2016-05-24 19:20:28 +0200

    working on repositories

---
+++ ConsoleApplication1/Repositories/GenericRepositori.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public void Delete(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetByID(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
