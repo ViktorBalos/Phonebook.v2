c25d4a48bb23f419ad493eff7f8527412340bd93
Alen <Alen@BRKA>
2016-05-24 19:14:23 +0200

    OpenProject

---
+++ ConsoleApplication1/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> Get();
+        T GetByID(int ID);
+        void Insert(T entity);
+        void Delete(int ID);
+        void Update(T entity);
+        void Save();
+    }
+}
