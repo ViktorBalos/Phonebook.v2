ad9a3c64f5a81da42b5a651a2c9ef2fcd0f4c32a
Alen <Alen@BRKA>
2016-06-08 20:44:48 +0200

    MVC Models

---
+++ Phonebook.v2.Web/Models/CountryModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Phonebook.v2.Web.Models
+{
+    public class CountryModel
+    {
+        public string CountryName { get; set; }
+    }
+}
